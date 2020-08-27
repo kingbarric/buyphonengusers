151cb87671a778172164bdde02e706c05878f400
sahmmie <smarsatto@gmail.com>
2020-08-28 00:46:57 +0100

    fixes

--- src/app/modules/blocks/block-brands/block-brands.component.ts
+++ src/app/modules/blocks/block-brands/block-brands.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { DirectionService } from '../../../shared/services/direction.service';
 import { RootService } from '../../../shared/services/root.service';
 import { Brand } from '../../../shared/interfaces/brand';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
     selector: 'app-block-brands',
@@ -11,11 +12,12 @@ import { Brand } from '../../../shared/interfaces/brand';
 export class BlockBrandsComponent {
     @Input() brands: Brand[] = [];
 
-    carouselOptions = {
+    carouselOptions:OwlOptions = {
         items: 6,
         nav: false,
-        dots: false,
+        // dots: true,
         loop: true,
+        lazyLoad:true,
         responsive: {
             1100: {items: 6},
             920: {items: 5},
@@ -23,7 +25,8 @@ export class BlockBrandsComponent {
             500: {items: 3},
             0: {items: 2}
         },
-        rtl: this.direction.isRTL()
+        rtl: this.direction.isRTL(),
+        navText : ['<div class=""><i class="fa fa-angle-left position-absolute" aria-hidden="true"></i>','<i class="fa fa-angle-right position-absolute" aria-hidden="true"></i></div>'],
     };
 
     constructor(
